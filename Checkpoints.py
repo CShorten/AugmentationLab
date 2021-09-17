2721422374460ea220e775b18546f2630a704da0
Connor Shorten <connorshorten300@gmail.com>
2021-09-17 10:35:15 -0400

    Update Checkpoints.py

--- Checkpoints.py
+++ Checkpoints.py
@@ -1,1 +1,32 @@
+# Take the master file as an argument and update it with the latest results
+def create_file_header(master_file, file_name,  eval_aug_list):
+  headings_row = []
+  headings_row.append("Epoch")
+  headings_row.append("Training Aug")
+  headings_row.append("Original Train Acc.")
+  headings_row.append("Original Test Acc.")
+  for aug in eval_aug_list:
+    headings_row.append(aug)
 
+  
+# add aug training strategy
+def get_model_results(model, epoch, training_aug, eval_aug_list,
+                      x_train, y_train, x_test, y_test,
+                      master_file):
+  print("evaluating...")
+  new_results_row = []
+  new_results_row.append(epoch)
+  new_results.append(training_aug)
+  new_results_row.append(model.evaluate(x_train, y_train)[1])
+  new_results_row.append(model.evaluate(x_test, y_test)[1])
+  
+  for aug in eval_aug_list:
+    aug_test = aug(images=x_test)
+    new_results.append(model.evaluate(aug_test, y_test)[1])
+  
+
+def save_file(master_file, file_name):
+  with open(file_name, mode='w') as data_file:
+    data_writer = csv.writer(data_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    for row in master_file:
+      data_writer.writerow(row)
