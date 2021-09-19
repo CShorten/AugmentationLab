e4d452efd7492e1d9000e072bbe57c9880719a08
Connor Shorten <connorshorten300@gmail.com>
2021-09-18 21:00:24 -0400

    Update CurriculumTraining.py

--- CurriculumTraining.py
+++ CurriculumTraining.py
@@ -17,7 +17,7 @@ def static_switching(model, static_curriculum, static_curriculum_names,
       master_file.append(get_model_results(model,
                                            str((i+1)*(j+1)*10),
                                            training_aug_name, aug_list,
-                                           x_train, y_train, x_test, y_test)
+                                           x_train, y_train, x_test, y_test))
   # add saving the model
   save_file(master_file, save_file_name)
         
