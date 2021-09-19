3a5ef5ca380e5c848cd9bce1575545e381b4a0b2
Connor Shorten <connorshorten300@gmail.com>
2021-09-18 20:37:39 -0400

    Update CurriculumTraining.py

--- CurriculumTraining.py
+++ CurriculumTraining.py
@@ -8,6 +8,7 @@ def static_switching(model, static_curriculum, static_curriculum_names,
   master_file.append(create_file_header(aug_names))
   for i in range(len(static_curriculum)):
     training_aug = static_curriculum[i]
+    training_aug_name = static_curriculum_names[i]
     # todo, change these to function arguments
     for j in range(5):
       for k in range(10):
@@ -15,7 +16,7 @@ def static_switching(model, static_curriculum, static_curriculum_names,
         model.fit(augmented_images, y_trian, batch_size=256, epochs=1)
       master_file.append(get_model_results(model,
                                            str((i+1)*(j+1)*10),
-                                           training_aug, aug_list,
+                                           training_aug_name, aug_list,
                                            x_train, y_train, x_test, y_test)
   save_file(master_file, save_file_name)
         
