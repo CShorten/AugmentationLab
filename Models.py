8304f88307f8fd85871619ef85d9c57c1d909df7
Connor Shorten <connorshorten300@gmail.com>
2021-09-20 08:55:30 -0400

    Update Models.py

--- Models.py
+++ Models.py
@@ -50,78 +50,103 @@ def compile_model(model, lr=0.001):
     ],
   )
 
-class Aug_Multiplicity_Model(keras.Model):
-  def __init__(self, model):
-    super(Aug_Multiplicity_Model, self).__init__()
-    self.model = model
-
-  def train_step(self, data):
-    [org_x, aug_1, aug_2, aug_3, aug_4], y = data
-    '''
-    # re-arrange this so you zip the data and parse it here
-    # e.g. org_x, y = org
-    # e.g. aug_1, aug_2, aug_3, aug_4 = augs
-    '''
-    with tf.GradientTape() as tape:
-        org_pred = self(org_x, training=True)
-        aug_pred_1 = self(aug_1, training=True)
-        aug_pred_2 = self(aug_2, training=True)
-        aug_pred_3 = self(aug_3, training=True)
-        aug_pred_4 = self(aug_4, training=True)
-
-        loss = self.compiled_loss(y, org_pred,
-                                   regularization_losses=self.losses)
-        loss += self.compiled_loss(y, aug_pred_1,
-                                   regularization_losses=self.losses)
-        loss += self.compiled_loss(y, aug_pred_2,
-                                   regularization_losses=self.losses)
-        loss += self.compiled_loss(y, aug_pred_3,
-                                   regularization_losses=self.losses)
-        loss += self.compiled_loss(y, aug_pred_4,
-                                   regularization_losses=self.losses)
-
-    trainable_vars = self.trainable_variables
-    gradients = tape.gradient(loss, trainable_vars)
-    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-
-    self.compiled_metrics.update_state(y, org_pred)
-    return {m.name: m.result() for m in self.metrics}
+# Vision Transformer
+# implementation from Khalid Salama, cite: https://keras.io/examples/vision/image_classification_with_vision_transformer/
+def create_vit_classifier(x_train):
+  import tensorflow_addons as tfa            
+  patch_size = 6  # Size of the patches to be extract from the input images
+  num_patches = (image_size // patch_size) ** 2
+  projection_dim = 64
+  num_heads = 4
+  transformer_units = [
+      projection_dim * 2,
+      projection_dim,
+  ]  # Size of the transformer layers
+  transformer_layers = 8
+  mlp_head_units = [2048, 1024]  # Size of the dense layers of the final classifier
   
-  def call(self, data):
-    return self.model(data)
-  
-def aug_multiplicity_model(x_train):
-  model = standard_model(x_train)
-  return Aug_Multiplicity_Model(model)
+  def mlp(x, hidden_units, dropout_rate):
+    for units in hidden_units:
+        x = layers.Dense(units, activation=tf.nn.gelu)(x)
+        x = layers.Dropout(dropout_rate)(x)
+    return x
 
-class Consistency_Model(keras.Model):
-  def __init__(self, model):
-    super(Consistency_Model, self).__init__()
-    self.model = model
+  class Patches(layers.Layer):
+    def __init__(self, patch_size):
+        super(Patches, self).__init__()
+        self.patch_size = patch_size
 
-  def train_step(self, data):
-    [org_data, aug_pair], y = data
+    def call(self, images):
+        batch_size = tf.shape(images)[0]
+        patches = tf.image.extract_patches(
+            images=images,
+            sizes=[1, self.patch_size, self.patch_size, 1],
+            strides=[1, self.patch_size, self.patch_size, 1],
+            rates=[1, 1, 1, 1],
+            padding="VALID",
+        )
+        patch_dims = patches.shape[-1]
+        patches = tf.reshape(patches, [batch_size, -1, patch_dims])
+        return patches     
+                   
+  class PatchEncoder(layers.Layer):
+    def __init__(self, num_patches, projection_dim):
+          super(PatchEncoder, self).__init__()
+          self.num_patches = num_patches
+          self.projection = layers.Dense(units=projection_dim)
+          self.position_embedding = layers.Embedding(
+              input_dim=num_patches, output_dim=projection_dim
+          )
 
-    with tf.GradientTape() as tape:
-      y_pred = self(org_data, training=True)
+    def call(self, patch):
+          positions = tf.range(start=0, limit=self.num_patches, delta=1)
+          encoded = self.projection(patch) + self.position_embedding(positions)
+          return encoded
+     
+  inputs = layers.Input(shape=input_shape)
+                   
+  data_augmentation = keras.Sequential(
+    [
+        layers.Normalization(),
+        layers.Resizing(image_size, image_size),
+    ],
+    name="data_augmentation",
+  )
+  # Compute the mean and the variance of the training data for normalization.
+  data_augmentation.layers[0].adapt(x_train)
+                   
+  # Augment data.
+  augmented = data_augmentation(inputs)
+  # Create patches.
+  patches = Patches(patch_size)(augmented)
+  # Encode patches.
+  encoded_patches = PatchEncoder(num_patches, projection_dim)(patches)
 
-      aug_pred = self(aug_pair, training=True)
+  # Create multiple layers of the Transformer block.
+  for _ in range(transformer_layers):
+      # Layer normalization 1.
+      x1 = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+      # Create a multi-head attention layer.
+      attention_output = layers.MultiHeadAttention(
+            num_heads=num_heads, key_dim=projection_dim, dropout=0.1
+      )(x1, x1)
+      # Skip connection 1.
+      x2 = layers.Add()([attention_output, encoded_patches])
+      # Layer normalization 2.
+      x3 = layers.LayerNormalization(epsilon=1e-6)(x2)
+      # MLP.
+      x3 = mlp(x3, hidden_units=transformer_units, dropout_rate=0.1)
+      # Skip connection 2.
+      encoded_patches = layers.Add()([x3, x2])
 
-      loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
-      # maybe want to re-weight these
-      loss += self.compiled_loss(y_pred, aug_pred, regularization_losses=self.losses)
-
-    trainable_vars = self.trainable_variables
-    gradients = tape.gradient(loss, trainable_vars)
-    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-
-    self.compiled_metrics.update_state(y, y_pred)
-    return {m.name: m.result() for m in self.metrics}
-  
-  def call(self, data):
-    return self.model(data)
-  
-def consistency_loss_model(x_train):
-  model = standard_model(x_train)
-  return Consistency_Model(model)
-
+  # Create a [batch_size, projection_dim] tensor.
+  representation = layers.LayerNormalization(epsilon=1e-6)(encoded_patches)
+  representation = layers.Flatten()(representation)
+  representation = layers.Dropout(0.5)(representation)
+  # Add MLP.
+  features = mlp(representation, hidden_units=mlp_head_units, dropout_rate=0.5)
+  # Classify outputs.
+  logits = layers.Dense(num_classes)(features)
+  # Create the Keras model.
+  model = keras.Model(inputs=inputs, outputs=logits)
+  return model
