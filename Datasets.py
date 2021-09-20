5e7428fd80456d6c98c1e7967eb1cc25465a6daf
Connor Shorten <connorshorten300@gmail.com>
2021-09-20 16:20:37 -0400

    Create Datasets.py

---
+++ Datasets.py
@@ -0,0 +1,6 @@
+def get_data():
+  from tensorflow import keras
+  (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+  y_train = keras.utils.to_categorical(y_train, 10)
+  y_test = keras.utils.to_categorical(y_test, 10)
+  return x_train, y_train, x_test, y_test
