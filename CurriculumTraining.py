5a2a4f21b05ca63e9605cfc530901e3582906d01
Connor Shorten <connorshorten300@gmail.com>
2021-09-21 13:02:31 -0400

    Update CurriculumTraining.py

--- CurriculumTraining.py
+++ CurriculumTraining.py
@@ -57,14 +57,41 @@ def AugSwitch(model, outer_epochs, inner_epochs,
         accuracy_difference_array[k] = abs(last_step_accuracy_array[k] - inner_epoch_results[k+4])
         last_step_accuracy_array[k] = inner_epoch_results[k+4] # 4 to offset (epoch, name, org_train, org_test)
     
-    # for testing, remove
-    print("Last Step Accuracy Array")
-    print(last_step_accuracy_array)
-    print("Accuracy Difference Array")
-    print(accuracy_difference_array)
-    
     epoch_counter += inner_epochs
   
   save_file(master_file, save_file_name)
-              
+
+'''
+The idea of Task Grouping is to use a lookahead comparison of the parameters updated with another task.
+Start with ø[t] and train it to candidate ø'[t+1] with each augmentation.
+Select the ø'[t+1] for ø[t+1] with the highest original test accuracy.
+'''
+def Task_Groupings(model, epochs,
+                   aug_list, aug_names
+                   x_train, y_train, x_test, y_test,
+                   save_file_name):
+  master_file = []
+  master_file.append(create_file_header(aug_names))
+  next_step_aug_idx = 0
+  for i in range(epochs):
+    training_aug = aug_list[next_step_aug_idx]
+    training_aug_name = aug_names[next_step_aug_idx]
+    print("===== Augmentation: " + training_aug_name + " =====")
+    model.save_weights("previous-step-weights.h5")
+    model_paths = []
+    for j in range(len(aug_list)):
+      model.load_weights("previous-step-weights.h5")
+      # train a candidate ø'[t+1] for each augmentation
+      training_aug = aug_list[j]
+      augmented_images = training_aug(images=x_train)
+      model.fit(augmented_images, y_train, batch_size=256, epochs=1)
+      model_save_path = aug_names[j] + "-candidate.h5"
+      model.save_weights(model_save_path)
+      model_paths.append(model_save_path)
+    inner_epoch_results, next_step_aug_idx = get_aug_results(i, model,
+                                                         model_paths, aug_name_list,
+                                                         x_test, y_test)
+    
+  
+  save_file(master_file, save_file_name)
         
