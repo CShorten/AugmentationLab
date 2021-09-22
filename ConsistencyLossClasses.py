66c78247dac5f3f41d419c4fff21bcbc60a1c193
Connor Shorten <connorshorten300@gmail.com>
2021-09-22 10:16:48 -0400

    Create ConsistencyLossClasses.py

---
+++ ConsistencyLossClasses.py
@@ -0,0 +1,65 @@
+class Consistency_Model(keras.Model):
+  def __init__(self, model):
+    super(Consistency_Model, self).__init__()
+    self.model = model
+
+  def train_step(self, data):
+    [org_data, aug_pair], y = data
+
+    with tf.GradientTape() as tape:
+      y_pred = self(org_data, training=True)
+
+      aug_pred = self(aug_pair, training=True)
+
+      loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+      # maybe want to re-weight these
+      loss += self.compiled_loss(y_pred, aug_pred, regularization_losses=self.losses)
+
+    trainable_vars = self.trainable_variables
+    gradients = tape.gradient(loss, trainable_vars)
+    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+    self.compiled_metrics.update_state(y, y_pred)
+    return {m.name: m.result() for m in self.metrics}
+  
+  def call(self, data):
+    return self.model(data)
+  
+def consistency_loss_model(x_train):
+  model = standard_model(x_train)
+  return Consistency_Model(model)
+
+# Second class so you can pass in the intermediate models
+# Need to add the RandAugment, y into this
+class Deep_Consistency_Model(keras.Model, intermediate_layer_models):
+  def __init__(self, model):
+    super(Consistency_Model, self).__init__()
+    self.model = model
+
+  def train_step(self, data):
+    [org_data, aug_pair], y = data
+
+    with tf.GradientTape() as tape:
+      y_pred = self(org_data, training=True)
+
+      aug_pred = self(aug_pair, training=True)
+
+      loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+      
+      # Consistency Losses
+      loss += self.compiled_loss(y_pred, aug_pred, regularization_losses=self.losses)
+
+      for i in range(intermediate_layer_models):
+        org_med = intermediate_layer_models[i].predict(org_data)
+        aug_med = intermediate_layer_models[i].predict(aug_data)
+        loss += self.compiled_loss(org_med, aug_med, regularization_losses=self.losses)
+        
+    trainable_vars = self.trainable_variables
+    gradients = tape.gradient(loss, trainable_vars)
+    self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+
+    self.compiled_metrics.update_state(y, y_pred)
+    return {m.name: m.result() for m in self.metrics}
+  
+  def call(self, data):
+    return self.model(data)
