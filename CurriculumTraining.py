52f77ab9351777acd2ca67801aba6e619f80070b
Connor Shorten <connorshorten300@gmail.com>
2021-09-22 16:00:18 -0400

    Update CurriculumTraining.py

--- CurriculumTraining.py
+++ CurriculumTraining.py
@@ -1,4 +1,4 @@
-from Checkpoints import create_file_header, create_task_groupings_header, get_model_results, get_aug_results, save_file
+from Checkpoints import create_file_header, get_model_results, save_file
 import numpy as np
 
 def static_switching(model, static_curriculum, static_curriculum_names,
@@ -58,41 +58,6 @@ def AugSwitch(model, outer_epochs, inner_epochs,
         last_step_accuracy_array[k] = inner_epoch_results[k+4] # 4 to offset (epoch, name, org_train, org_test)
     
     epoch_counter += inner_epochs
-  
-  save_file(master_file, save_file_name)
-
-'''
-The idea of Task Grouping is to use a lookahead comparison of the parameters updated with another task.
-Start with ø[t] and train it to candidate ø'[t+1] with each augmentation.
-Select the ø'[t+1] for ø[t+1] with the highest original test accuracy.
-'''
-def Task_Groupings(model, epochs,
-                   aug_list, aug_name_list,
-                   x_train, y_train, x_test, y_test,
-                   save_file_name):
-  master_file = []
-  master_file.append(create_task_groupings_header(aug_name_list))
-  winning_aug_idx = 0
-  for i in range(epochs):
-    training_aug = aug_list[winning_aug_idx]
-    training_aug_name = aug_name_list[winning_aug_idx]
-    print("===== Augmentation: " + training_aug_name + " =====")
-    model.save_weights("previous-step-weights.h5")
-    model_paths = []
-    for j in range(len(aug_list)):
-      model.load_weights("previous-step-weights.h5")
-      # train a candidate ø'[t+1] for each augmentation
-      training_aug = aug_list[j]
-      augmented_images = training_aug(images=x_train)
-      model.fit(augmented_images, y_train, batch_size=256, epochs=1)
-      model_save_path = aug_name_list[j] + "-candidate.h5"
-      model.save_weights(model_save_path)
-      model_paths.append(model_save_path)
-    inner_epoch_results, winning_aug_idx = get_aug_results(i, model,
-                                                         model_paths, aug_name_list,
-                                                         x_test, y_test)
-    model.load_weights(model_paths[winning_aug_idx])
-    
   
   save_file(master_file, save_file_name)
         
