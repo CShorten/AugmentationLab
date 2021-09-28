0f9ef44083e9ab0c1e3517b19864736de08c0787
Connor Shorten <connorshorten300@gmail.com>
2021-09-27 20:19:46 -0400

    Update Training.py

--- Training.py
+++ Training.py
@@ -2,7 +2,7 @@ def standard_training(model, meta_steps,
                       training_aug_list, testing_aug_list, 
                       x_train, y_train, x_test, y_test):
   for i in range(meta_steps):
-    print("===== Training Step: " + str((i+1)*10) + " =====")
+    print("===== Training Step: " + str(i*10) + " =====")
     for aug in training_aug_list:
       augmented_data = aug(images=x_train)
       model.fit(augmented_data, y_train, batch_size=256, epochs=1)
