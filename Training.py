985b378d870dcf571b750e8a5693d668426829c1
Connor Shorten <connorshorten300@gmail.com>
2021-10-01 11:18:04 -0400

    Update Training.py

--- Training.py
+++ Training.py
@@ -14,17 +14,16 @@ def standard_training(model, meta_steps,
       
 def few_shot_training(model, model_init_path,
                       outer_epochs, inner_epochs,
-                      aug_list, aug_names,
+                      aug_list, aug_names, negative_aug, negative_aug_name,
                       x_train,y_train,x_test,y_test):
   for i in range(len(aug_list)):
     model.load_weights(model_init_path)
     train_index = set(range(len(aug_list)))
-    held_out = i, (i+1)%len(aug_list)
-    held_out_aug_1, held_out_aug_2 = aug_list[held_out[0]], aug_list[held_out[1]]
-    train_index.remove(held_out[0])
-    train_index.remove(held_out[1])
+    held_out = i
+    held_out_aug = aug_list[held_out]
+    train_index.remove(held_out)
     print("\n")
-    print("Holding out: " + str(aug_names[held_out[0]]) + " and " + str(aug_names[held_out[1]]))
+    print("Holding out: " + str(aug_names[held_out]) + " and " + negative_aug_name)
     print("\n")
     train_index = list(train_index)
     for j in range(outer_epochs):
@@ -35,11 +34,11 @@ def few_shot_training(model, model_init_path,
           augmented_x = aug(images=x_train)
           model.fit(augmented_x, y_train, batch_size=256, epochs=1)
       print("Evaluating...")
-      print(aug_names[held_out[0]])
-      test_aug_1 = held_out_aug_1(images=x_test)
+      print(aug_names[held_out])
+      test_aug_1 = held_out(images=x_test)
       model.evaluate(test_aug_1, y_test)
-      print(aug_names[held_out[1]])
-      test_aug_2 = held_out_aug_2(images=x_test)
+      print(negative_aug_name)
+      test_aug_2 = negative_aug(images=x_test)
       model.evaluate(test_aug_2, y_test)
                       
       
