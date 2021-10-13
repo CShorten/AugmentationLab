359004be154e7d27800fbb3856a9b57aeb69bab5
Connor Shorten <connorshorten300@gmail.com>
2021-10-13 17:06:55 -0400

    Update DataLoaders.py

--- DataLoaders.py
+++ DataLoaders.py
@@ -3,6 +3,14 @@ import tensorflow as tf
 import imgaug.augmenters as iaa
 import math
 
+'''
+Work in Progress: Not Finished
+
+https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+
+Useful for storing datasets on the disk!
+
+'''
 class standard_aug_loader(tf.keras.utils.Sequence):
   def __init__(self, x, y, aug, batch_size):
     self.x, self.y = x, y
@@ -28,11 +36,12 @@ class org_aug_pair_loader(tf.keras.utils.Sequence):
     return batch_x, aug_batch, batch_y
 
 class org_aug_randaug_triple_loader(tf.keras.utils.Sequence):
-  def __init__(self, x, y, randaug, aug, batch_size):
+  def __init__(self, x, y, randaug, aug, dim=(32,32), n_channels=3, batch_size):
     self.x, self.y = x, y
     self.aug = aug
     self.batch_size = batch_size
     self.shuffle = True
+    self.on_epoch_end()
   
   def __getitem__(self, idx):
     batch_x = self.x[idx * self.batch_size: (idx + 1) * self.batch_size]
@@ -43,6 +52,11 @@ class org_aug_randaug_triple_loader(tf.keras.utils.Sequence):
  
   def on_epoch_end(self):
     # Updates indexes after each epoch
-    self.indexes = np.arange(len(self.list_IDs))
+    self.indexes = np.arange(len(self.x))
     if self.shuffle == True:
       np.random.shuffle(self.indexes)
+      
+  def __data_generation(self, list_IDs_temp):
+    # Generates data containing batch_size samples
+    # X : (n_samples, *dim, n_channels)
+    X = np.empty((
