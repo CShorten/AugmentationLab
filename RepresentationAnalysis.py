4c2bc143912046bb197831567ae3baab9326c81f
Connor Shorten <connorshorten300@gmail.com>
2021-10-14 11:46:27 -0400

    Update RepresentationAnalysis.py

--- RepresentationAnalysis.py
+++ RepresentationAnalysis.py
@@ -19,7 +19,7 @@ def contrast_distances(org_img2vec, aug_img2vec, same_class_as_org_img2vec):
 def get_vectors(model, dataset, data_dim):
   vector_reps = []
   for instance in dataset:
-    vector_reps.append(model.predict(instance).reshape(1, data_dim[0], data_dim[1], data_dim[2]))
+    vector_reps.append(model.predict(instance.reshape(1, data_dim[0], data_dim[1], data_dim[2]))
   return vector_reps
   
 def l1_vec_distance(vec1, vec2):
