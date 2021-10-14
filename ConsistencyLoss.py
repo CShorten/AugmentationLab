9decd84b70e36571a28fc26b784438c53c0f282c
Connor Shorten <connorshorten300@gmail.com>
2021-10-13 20:55:24 -0400

    Update ConsistencyLoss.py

--- ConsistencyLoss.py
+++ ConsistencyLoss.py
@@ -47,7 +47,7 @@ class Consistency_Model_with_RandAug(keras.Model):
       aug_pred = self(aug_pair, training=True) # could turn off training here BYOL style
       randaug_pred = self(randaug_pair, training=True) # could turn off training here BYOL style
 
-      loss = self.compiled_loss(y, randaug_pred, regularization_losses=self.losses
+      loss = self.compiled_loss(y, randaug_pred, regularization_losses=self.losses)
       loss += self.consistency_weight * self.compiled_loss(org_y_pred, aug_pred, regularization_losses=self.losses)
 
     trainable_vars = self.trainable_variables
