a881ec3d8b7c574e67e9e97a1794da3d019aecf3
Connor Shorten <connorshorten300@gmail.com>
2021-10-16 16:56:33 -0400

    Update RepresentationAnalysis.py

--- RepresentationAnalysis.py
+++ RepresentationAnalysis.py
@@ -2,18 +2,15 @@
 
 # High-Level Wrappers
 def distribution_analysis(dist1, dist2):
-  print("=======================")
-  print("Comparing Distributions")
-  print("Average Distance: " + str(get_average_vec_distance(dist1,dist2)))
-  print("=======================")
+  print(get_average_vec_distance(dist1,dist2))
 
-def contrast_distances(org_img2vec, aug_img2vec, same_class_as_org_img2vec):
-  print("===================================")
-  print("Original versus Augmented Distance")
-  print(l1_vec_distance(org_img2vec, aug_img2vec))
-  print("Original versus Same Class Original")
-  print(l1_vec_distance(org_img2vec, same_class_as_org_img2vec))
-  print("===================================")
+def get_average_vec_distance(dist1,dist2):
+  # just defaulting to l1 for now
+  # dist1
+  sum = 0
+  for i in range(len(dist1)):
+    sum += l1_vec_distance(dist1[i], dist2[i])
+  return sum / len(dist1)
   
 def l1_vec_distance(vec1, vec2):
   sum = 0
