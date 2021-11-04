0565a7f378cef7a9e2e84e596f72eadaa951136b
Connor Shorten <connorshorten300@gmail.com>
2021-11-04 13:00:37 -0400

    Update ImbalanceSimulator.py

--- ImbalanceSimulator.py
+++ ImbalanceSimulator.py
@@ -1,5 +1,25 @@
-def fixed_sampling(imbalance_pct, minority_label, xs, ys):
-  return 0
+import numpy as np
 
+def fixed_sampling(imbalance_pct, minority_label, xs, ys):
+  ret_xs = []
+  ret_ys = []
+  
+  minority_count = len(xs) * imbalance_pct
+  counter = 0
+  
+  for i, y in ys:
+      if counter > minority_count:
+        break
+      if (np.argmax(y[i]) == minority_label):
+        ret_xs.append(xs[i])
+        ret_ys.append(y)
+  
+  return ret_xs, ret_ys
+
+# extension of above to not sample left to right for the imbalance pct
 def random_sampling():
   return 0
+
+# Automatic Domain Randomizationw rapper of random_sampling
+def automatic_imbalance_randomization():
+  # calls the above functions for k steps
