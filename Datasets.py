ad5b8d6d8eb3db3e5586f3bdf3bb29d73421e2a8
Connor Shorten <connorshorten300@gmail.com>
2021-11-04 13:35:40 -0400

    Update Datasets.py

--- Datasets.py
+++ Datasets.py
@@ -1,6 +1,31 @@
+from tensorflow import keras
+  
 def get_cifar_10():
-  from tensorflow import keras
   (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
   y_train = keras.utils.to_categorical(y_train, 10)
   y_test = keras.utils.to_categorical(y_test, 10)
   return x_train, y_train, x_test, y_test
+
+def get_cifar10_deer_vs_horses():
+  (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+  # data filtering based on labeling
+  filtered_x_train, filtered_y_train = [], []
+  for i, y in enumerate(y_train):
+    if (y == 4):
+      filtered_x_train.append(x_train[i])
+      filetered_y_train.append(y)
+    if (y == 7):
+      filtered_x_train.append(x_train[i])
+      filtered_y_train.append(y)
+      
+  for i, y in enumerate(y_test):
+    if (y == 4):
+      filtered_x_train.append(x_train[i])
+      filetered_y_train.append(y)
+    if (y == 7):
+      filtered_x_train.append(x_train[i])
+      filtered_y_train.append(y)
+      
+  filtered_y_train = keras.utils.to_categorical(filtered_y_train, 10)
+  filtered_y_test = keras.utils.to_categorical(filtered_y_test, 10)
+  return filtered_x_train, filtered_y_train, filtered_x_test, filtered_y_test
